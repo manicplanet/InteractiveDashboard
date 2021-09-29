f004a38d5b1bc90f5d81b4b0fd7124a6ccfb43dd
Jonathan <jonmtalbot@gmail.com>
2021-09-29 09:30:29 -0400

    Update ui.R
    edited inputID sections

--- ui.R
+++ ui.R
@@ -13,7 +13,7 @@ shinyUI(
     # TASK 2: Add first fluidRow to select input for country
     fluidRow(
       column(12, 
-             wellPanel(selectInput(inputid = "country", label = "country",choices = c("United-States", "Canada", "Mexico", "Germany", "Philippines"))) #country
+             wellPanel(selectInput(inputId = "country", label = "country",choices = c("United-States", "Canada", "Mexico", "Germany", "Philippines"))) #country
              )
     ),
     #
@@ -22,8 +22,8 @@ shinyUI(
       column(3, 
              wellPanel(
                p("Select a continuous variable and graph type (histogram or boxplot) to view on the right."),
-               radioButtons(continuous_variable, label = "Continuous Variable", choices = c("age","hours_per_week")),
-               radioButtons(graph_type, label = "Graph Type", choices = c("histogram","boxplot"))
+               radioButtons(inputId = "continuous_variable", label = "Continuous Variable", choices = c("age","hours_per_week")),
+               radioButtons(inputId = "graph_type", label = "Graph Type", choices = c("histogram","boxplot"))
                )
              ),
       column(9, plotOutput("p1"))  # p1
@@ -34,9 +34,9 @@ shinyUI(
       column(3, 
              wellPanel(
                p("Select a categorical variable to view bar chart on the right. Use the check box to view a stacked bar chart to combine the income levels into one graph. "),
-               radioButtons(inputid = "categorical_variable", label = "Demographic", choices = c("education", "workclass", "sex")
+               radioButtons(inputId = "categorical_variable", label = "Demographic", choices = c("education", "workclass", "sex")
                ),    
-               checkboxInput(is_stacked),     # if the bars "is_stacked" (input ID), that is, if the checkbox is checked then the bars will be stacked. Otherwise, they will be faceted and unstacked. Add a label. The initial value can be either FALSE or TRUE. 
+               checkboxInput(inputID = "is_stacked", label = "Is stacked"),     # if the bars "is_stacked" (input ID), that is, if the checkbox is checked then the bars will be stacked. Otherwise, they will be faceted and unstacked. Add a label. The initial value can be either FALSE or TRUE. 
                )
              ),
       column(9,  plotOutput("p2"))  # 
